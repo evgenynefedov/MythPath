bae39e1129d2d8c42d10b3ec1d5414be2ecdc6fe
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-31 19:58:23 +0300

    Implement stroing keys in .env

---
+++ frontend/src/constants.js
@@ -0,0 +1,24 @@
+export const CONSTANTS = {
+  /**
+ * Values are read from .env file, which  should be in React project root dir (/fronend/)
+ * .env is marked as untracked in .gitignotre
+ * .env contains pairs in `KEY = VALUE` format
+ * All keys shoud start with `REACT_APP_`
+ * After setting or changing the keys in  `/.env` stop running project and run `npm start`
+ * again.
+ * .env shoud have the following lines:
+ *@example 
+REACT_APP_OPENAI_API_KEY = hereIsSomeApiKey
+REACT_APP_CLOUDINARY_API_KEY  = hereIsSomeOtherApiKey
+ */
+  apiKeys: {
+    /**
+     * openai.com
+     */
+    openai: `${process.env.REACT_APP_OPENAI_API_KEY}`,
+    /**
+     * cloudinary.com
+     */
+    cloudinary: `${process.env.REACT_APP_CLOUDINARY_API_KEY}`,
+  },
+};
