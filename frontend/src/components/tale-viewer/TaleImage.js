0c836f2bb4dd1301f5e69efc05d7312a6bd10e70
Dmitry Sokolov <urbi@varenye.com>
2023-09-05 10:20:54 +0300

    adeed temporary styles to image

--- frontend/src/components/tale-viewer/TaleImage.js
+++ frontend/src/components/tale-viewer/TaleImage.js
@@ -1,9 +1,12 @@
-import React from "react";
-
 export const TaleImage = ({ src, alt }) => (
   <img
     src={src}
     alt={alt}
-    style={{ width: "100%", objectFit: "cover", flex: "1" }}
+    style={{
+      width: "100%",
+      objectFit: "cover",
+      flex: "1",
+      maxHeight: "50vh", // ограничиваем высоту
+    }}
   />
 );
