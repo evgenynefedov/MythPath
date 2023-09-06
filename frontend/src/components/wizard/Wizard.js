e2cc8387e0f38e070d65a13572ebfdc4e534668c
evgenynefedov <e.nefyodoff@gmail.com>
2023-09-06 15:13:18 +0300

    taleLoader integration in wizard

--- frontend/src/components/wizard/Wizard.js
+++ frontend/src/components/wizard/Wizard.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import NavBar from "./NavBar";
 import SpellSelector from "./SpellSelector";
+import TaleLoader from "../tale-loader/TaleLoader";
 
 export default function Wizard() {
   const navigate = useNavigate();
@@ -37,6 +38,8 @@ export default function Wizard() {
   const [stepIndex, setStepIndex] = useState(0);
   const [steps, setSteps] = useState(STEPS);
   const [spells, setSpells] = useState(false);
+
+  const [isLoading, setIsloading] = useState(false);
 
   const makeStep = (shift) => {
     const newStepIndex = stepIndex + shift;
@@ -98,14 +101,18 @@ export default function Wizard() {
 
   const generateTale = async function (storyParameters) {
     let story = false;
-    story = await textGenerator(storyParameters);
-    if (story) {
-      console.log("story was generated with params:");
-      console.log(storyParameters);
-      navigate("/tale-viewer/0");
-    } else {
-      console.log("something wrong in generating of the story");
-    }
+    setIsloading(true);
+    //TO DO: delete setTimeout (it's only to show loader now)
+    setTimeout(async () => {
+      story = await textGenerator(storyParameters);
+      if (story) {
+        console.log("story was generated with params:");
+        console.log(storyParameters);
+        navigate("/tale-viewer/0");
+      } else {
+        console.log("something wrong in generating of the story");
+      }
+    }, 3000);
   };
 
   useEffect(() => {
@@ -113,27 +120,33 @@ export default function Wizard() {
   }, [stepIndex]);
 
   return (
-    <Container>
-      <NavBar
-        stepsCount={stepsCount}
-        stepIndex={stepIndex}
-        isSelected={steps[stepIndex].value !== "undefined" ? true : false}
-        back={() => {
-          makeStep(-1);
-        }}
-        next={() => {
-          makeStep(1);
-        }}
-      />
+    <>
+      {isLoading ? (
+        <TaleLoader />
+      ) : (
+        <Container>
+          <NavBar
+            stepsCount={stepsCount}
+            stepIndex={stepIndex}
+            isSelected={steps[stepIndex].value !== "undefined" ? true : false}
+            back={() => {
+              makeStep(-1);
+            }}
+            next={() => {
+              makeStep(1);
+            }}
+          />
 
-      {spells && (
-        <SpellSelector
-          spells={spells}
-          step={steps[stepIndex]}
-          isMultiselector={steps[stepIndex].isMulti}
-          updateStep={updateStep}
-        />
+          {spells && (
+            <SpellSelector
+              spells={spells}
+              step={steps[stepIndex]}
+              isMultiselector={steps[stepIndex].isMulti}
+              updateStep={updateStep}
+            />
+          )}
+        </Container>
       )}
-    </Container>
+    </>
   );
 }
