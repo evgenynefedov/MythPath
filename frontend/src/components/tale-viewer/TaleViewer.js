2e8703d092affdf2cae0537686e64071763ac583
evgenynefedov <e.nefyodoff@gmail.com>
2023-09-10 14:55:51 +0300

    multi stories storage service implemented

--- frontend/src/components/tale-viewer/TaleViewer.js
+++ frontend/src/components/tale-viewer/TaleViewer.js
@@ -1,31 +1,34 @@
-import "./TaleViewer.css"
+import "./TaleViewer.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Box, Paper } from "@mui/material";
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import { useSwipeable } from "react-swipeable";
 // import { textGenerator } from "../../services/textGenerator";
 // import storyTemplate from "../../Data/fantasy_story_template.json";
 // import { splitStoryIntoPages } from "../../Utils/splitIntoPages";
-import * as taleStorage from "./../../services/taleStorage";
+import * as taleStorage from "../../services/taleStorage";
 import TalePage from "./TalePage";
 // import { TaleImage } from "./TaleImage";
 // import { TaleText } from "./TaleText";
 import { TaleControls } from "./TaleControls";
+import { useParams } from "react-router";
 
 export default function TaleViewer() {
+  const { taleId } = useParams();
+
   const [currentPage, setCurrentPage] = useState(0);
   const [story, setStory] = useState({});
 
   useEffect(() => {
     async function fetchStory() {
-      //const newStory = await textGenerator(storyTemplate);
-      const newStory = await taleStorage.getTale();
-      setStory(newStory);
-      console.log(newStory)
+      const newStory = await taleStorage.getTaleById(taleId);
+      setStory(newStory ? newStory.tale : {});
     }
     fetchStory();
   }, []);
+
+  useEffect(() => {}, [story]);
 
   const handleNext = () =>
     setCurrentPage((prev) => Math.min(story.pages.length - 1, prev + 1));
@@ -36,25 +39,27 @@ export default function TaleViewer() {
     onSwipedRight: () => handlePrevious(),
   });
   return (
-    <Paper elevation={3} style={{ height: "100vh"}}>
-      <Box
-        {...handlers}
-        className="tale_container"
-      >
-        {story.pages && story?.pages?.length !== 0 &&
+    <Paper elevation={3} style={{ height: "100vh" }}>
+      <Box {...handlers} className="tale_container">
+        {story.pages && story?.pages?.length !== 0 && (
           <>
             <TalePage page={story.pages[currentPage]} tittle={story.title} />
-            <TaleControls 
-              handlePrevious={handlePrevious} 
-              handleNext={handleNext} 
-              steps={story.pages.length} currentPage={currentPage} />
+            <TaleControls
+              handlePrevious={handlePrevious}
+              handleNext={handleNext}
+              steps={story.pages.length}
+              currentPage={currentPage}
+            />
           </>
-        }
-        <Link to="/" style={{
-              position: "absolute",
-              top: "20px",
-              left: "20px",
-            }} >
+        )}
+        <Link
+          to="/"
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "20px",
+          }}
+        >
           <HighlightOffIcon />
         </Link>
       </Box>
