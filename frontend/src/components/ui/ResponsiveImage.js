f8ae2ab03c1800d02a21eddcf1688ff4da97baa3
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-09-12 10:15:46 +0300

    Add resize step, aspectRation now optional

--- frontend/src/components/ui/ResponsiveImage.js
+++ frontend/src/components/ui/ResponsiveImage.js
@@ -3,22 +3,25 @@ import { getCloudinaryImage } from "../../services/cloudinary";
 import { AdvancedImage, lazyload, responsive } from "@cloudinary/react";
 import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 
+const STEP_PIXELS = 100;
+
 /**
  * Wrapper component for cloudinary AdvancedImage, returns image resized to aspectRation, with responsive auto sizing to parent
  * @param {*} props
  * @param {string} props.imgPath path to image file, starting from MythPath/Library
- * @param {number} aspectRatio
+ * @param {number?} props.aspectRatio aspectRatio
  * @returns
  */
 export default function ResponsiveImage({ imgPath, aspectRatio }) {
   const cloudinaryImage = getCloudinaryImage(imgPath).resize(
-    fill().aspectRatio(aspectRatio).gravity(autoGravity())
+    aspectRatio
+      ? fill().aspectRatio(aspectRatio).gravity(autoGravity())
+      : fill().gravity(autoGravity())
   );
-
   return (
     <AdvancedImage
       cldImg={cloudinaryImage}
-      plugins={[responsive(), lazyload()]}
+      plugins={[responsive(STEP_PIXELS), lazyload()]}
     />
   );
 }
