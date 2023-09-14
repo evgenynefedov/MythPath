dd98d0cbdba10d8244d3555742a57552555254b4
Pavel Kondratovich <kondratovich@ro.ru>
2023-09-14 11:19:52 +0300

    added quotes and used random func

--- frontend/src/components/tale-loader/TaleLoader.js
+++ frontend/src/components/tale-loader/TaleLoader.js
@@ -1,7 +1,25 @@
 import * as React from "react";
-import { Box, Container, LinearProgress } from "@mui/material";
+import { Box, Container, LinearProgress, Typography } from "@mui/material";
+import "./TaleLoader.css";
+import getRandomElementFromArray from "../../Utils/getRandomElementFromArray";
 
 export default function TaleLoader() {
+  const quotes = [
+    "The loading screen is the real villain in this story.",
+    "If I had a magic wand, I'd use it to speed up this loading time.",
+    "This loading time is longer than most fairy tale marriages.",
+    "Once upon a time in a land far, far away, there was a loading bar that never reached its end",
+    "I asked for a fairy tale, not a never-ending story of loading.",
+    "In the time it's taking to load, I could have rescued the princess myself.",
+    "Loading... Because even fairy godmothers need a coffee break.",
+    "If this loading time were a witch, it'd be a wicked one.",
+    "This loading bar is like Cinderella's carriage—it only lasts until midnight.",
+    "In the time it's taking, I could have kissed a frog and built a castle.",
+    "Rapunzel, let down your loading bar!",
+    "Waiting for this fairy tale feels like I'm stuck in the 'Tower of Endless Loading.'",
+    "Once upon a time, in a land filled with loading screens, our hero waited.",
+  ];
+
   return (
     <Box
       sx={{
@@ -9,22 +27,19 @@ export default function TaleLoader() {
         minHeight: "100vh",
         alignItems: "center",
         flexGrow: 1,
+        textAlign: "center",
       }}
     >
       <Container maxWidth="sm">
+        <Typography variant="h3" fontFamily="Kranky">
+          {getRandomElementFromArray(quotes)}
+        </Typography>
         <Box
           sx={{
             display: "flex",
             justifyContent: "center",
           }}
-        >
-          <img
-            src="https://images.squarespace-cdn.com/content/v1/520eab84e4b02d5660581bbb/1560883857233-4NYNW6AOSYY6RIMDTMWW/matt-anderson-canopy-wikia-fandom-dark-fairytales-hero-banner.png?format=2500w"
-            // nice to have hear a picture of the current loading world
-            width={450}
-            alt="pic"
-          />
-        </Box>
+        ></Box>
         <LinearProgress sx={{ mt: 4 }} />
       </Container>
     </Box>
