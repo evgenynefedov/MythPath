5f0632e68497362b0e5d3c1b79acb1707d069a4c
Dmitry Sokolov <urbi@varenye.com>
2023-09-15 16:53:17 +0300

    delete console.log

--- frontend/src/services/libraryStorage.js
+++ frontend/src/services/libraryStorage.js
@@ -1,6 +1,6 @@
 import StoryParamsConfig from "../Data/storyParamsConfig.json";
 
-export async function getLibraryData(stepCode, languageCode, worldId = null) {
+export async function getLibraryData(stepCode, languageCode) {
   if (!StoryParamsConfig || !StoryParamsConfig.steps) {
     throw new Error(
       "StoryParamsConfig or StoryParamsConfig.steps is undefined"
@@ -10,10 +10,10 @@ export async function getLibraryData(stepCode, languageCode, worldId = null) {
   if (!step) {
     throw new Error(`Invalid step code: ${stepCode}`);
   }
-  return getLibraryItems(step.libraryPath, languageCode, worldId);
+  return getLibraryItems(step.libraryPath, languageCode);
 }
 
-async function getLibraryItems(path, languageCode = "en", worldId = null) {
+async function getLibraryItems(path, languageCode = "en") {
   async function fetchJson(url) {
     const response = await fetch(url);
     return await response.json();
@@ -21,20 +21,16 @@ async function getLibraryItems(path, languageCode = "en", worldId = null) {
 
   let items = await fetchJson(`/library/${path}.json`);
 
-  console.log(`/library/${path}.${languageCode}.json`);
   const itemsText = await fetchJson(`/library/${path}.${languageCode}.json`);
 
   const itemTextById = new Map(
     itemsText.map((charText) => [charText.id, charText])
   );
 
-  console.log(itemsText);
-
   items = items.map((char) => ({
     ...char,
     ...itemTextById.get(char.id),
   }));
-  console.log(items);
 
   return items;
 }
