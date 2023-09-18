5c585244fcdd19c3ef48eb92e60d3d4ed4eb469d
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-09-18 17:38:33 +0300

    Add skipping Lib prefix in custom picture paths

--- frontend/src/services/cloudinary.js
+++ frontend/src/services/cloudinary.js
@@ -5,16 +5,27 @@ const EXTENSION_REGEX = /\.[^/.]+$/;
 const removeExtension = (filePath) => filePath?.replace(EXTENSION_REGEX, "");
 
 /**
+ * @param {string} imgPath
+ * @returns {string}
+ * @example getCloudinaryPath("v1694964113/user/fipejlgks6jxl9lya3qp.png") === "v1694964113/user/fipejlgks6jxl9lya3qp.png"
+ * getCloudinaryPath("/Worlds/pawfect.jpg") === "MythPath/Library/Worlds/pawfect"
+ */
+const getCloudinaryPath = (imgPath, isLibrary) =>
+  isLibrary
+    ? `${CONSTANTS.cloudinaryBasePath}${removeExtension(imgPath)}`
+    : removeExtension(imgPath);
+
+/**
  * Create CloudinaryImage object for image in MythPath/Library with q_auto anf f_auto
  * @param imgPath path to file, starting from MythPath/Library
  * @returns CloudinaryImage
  */
-export const getCloudinaryImage = (imgPath) =>
-  new CloudinaryImage(
-    `${CONSTANTS.cloudinaryBasePath}${removeExtension(imgPath)}`,
-    {
-      cloudName: CONSTANTS.cloudinaryCloudName,
-    }
-  )
-    .quality("auto")
-    .format("auto");
+export const getCloudinaryImage = (
+  imgPath,
+  isLibrary = imgPath.startsWith("/")
+) =>
+  new CloudinaryImage(getCloudinaryPath(imgPath, isLibrary), {
+    cloudName: CONSTANTS.cloudinaryCloudName,
+  }).quality("auto");
+// doesn't work on Chrome emulation of mobile
+//    .format("auto")
